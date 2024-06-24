3e8071588e6665e619786481ed4399da1b58d950
MykolaDekusar <ndekusar97@gmail.com>
2024-06-24 18:19:32 +0200

    [UPD]Main exercise completed

--- index.php
+++ index.php
@@ -1,4 +1,33 @@
 <?php
+// creating a class
 class Movie
 {
+    // setting private info of class
+    private string $title;
+    private string $genre;
+    private int $year;
+    // constructor function 
+    public function __construct(string $title, string $genre, int $year)
+    {
+        // handle of empty info
+        if (empty($title) || empty($genre)) {
+            throw new Exception("Fill all camps");
+        } else {
+            // assigning values
+            $this->title = $title;
+            $this->genre = $genre;
+            $this->year = $year;
+        }
+    }
+    // function to get movie info
+    public function getFilm(): string
+    {
+        return "$this->title $this->genre $this->year";
+    }
 }
+
+$movie1 = new Movie('Avengers', 'Action', 2014);
+$movie2 = new Movie('Matrix', 'Action', 1998);
+echo $movie1->getFilm();
+echo "<br>";
+echo $movie2->getFilm();
